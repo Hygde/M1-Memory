ffa99c5eb5c5e7e71c3ec31665028a486e9f34f3
Hygde <samuel.devals@outlook.fr>
2017-11-15 16:46:51 +0100

    Updating FileManagement class, and upgrading UI in SettingsActivity

--- app/src/main/java/projetandroidmaster1/memory/FileManagement.java
+++ app/src/main/java/projetandroidmaster1/memory/FileManagement.java
@@ -2,16 +2,15 @@ package projetandroidmaster1.memory;
 
 import android.content.Context;
 import android.util.Log;
-import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Collections;
 
 /**
  * Created by Samuel on 13/11/2017.
@@ -19,89 +18,106 @@ import java.util.Collections;
 
 public class FileManagement {
 
+    private final String AppSettingsFileName = "AppSettings.txt";
     private final String ScoreFileName = "score.txt";
     private final String GameStateFile = "gamestate.txt";
     private final int ERROR = -1;
 
+    private static File AppSettings;
+    private static File FScore;
+    private static File FGameState;
 
     private Context ctx;
 
+    //constructor
     public FileManagement(Context ctx){
         this.ctx = ctx;
-
-        try {
-            File f = new File("home/user/Documents/"+ScoreFileName);
-            Toast.makeText(ctx,f.getAbsolutePath(),Toast.LENGTH_LONG).show();
-            if(!f.exists()) {
-                f.createNewFile();
-                Log.e("Memory : ", "FileManagement() : Creating file "+ScoreFileName);
-            }
-        } catch (IOException e) {
-            Log.e("Memory : ", "FileManagement() : Error while creating "+ScoreFileName);
-        }
-
-        try{
-            File f1 = new File(ctx.getFilesDir(), GameStateFile);
-            if(!f1.exists()){
-                f1.createNewFile();
-                Log.e("Memory : ", "FileManagement() : Creating file "+GameStateFile);
-            }
-        }catch(IOException e){
-            Log.e("Memory : ", "FileManagement() : Error while creating "+GameStateFile);
-        }
+        AppSettings = InitFile(ctx, AppSettingsFileName);
+        FScore = InitFile(ctx, ScoreFileName);
+        FGameState = InitFile(ctx, GameStateFile);
     }
 
-    public int WriteScore(String score){
-        int result = 0;
-        Log.e("Memory : ","WriteScore() : "+score);
+    //create file if not exist
+    private File InitFile(Context ctx, String FName){
+        File f;
         try{
-            OutputStreamWriter out = new OutputStreamWriter(ctx.openFileOutput(ScoreFileName,Context.MODE_PRIVATE));
-            BufferedWriter bw = new BufferedWriter(out);
-            bw.write(score);
-            bw.newLine();
-            bw.flush();
-            bw.close();
-            out.close();
+            f = new File(ctx.getFilesDir(), FName);
+            if(!f.exists())f.createNewFile();
         }catch(IOException e){
-            Log.e("Memory : ", "WriteScore() : Error while write score");
+            Log.e("MEMORY : ", "FileManagement.InitFile() : Error while creating "+FName);
+            f = null;
+        }
+        return f;
+    }
+
+    //Write a line in File
+    private int WriteLn(File file,String str){
+        Log.e("MEMORY : ", "FileManagement.WriteLn() : "+str+" into "+file.getName());
+        int result = 0;
+        try{
+            FileOutputStream fout = new FileOutputStream(file);
+            OutputStreamWriter wfout = new OutputStreamWriter(fout);
+            BufferedWriter bwfout = new BufferedWriter(wfout);
+            bwfout.write(str);
+            bwfout.newLine();
+            bwfout.flush();
+            bwfout.close();
+        }catch(IOException e){
+            Log.e("MEMORY : ", "FileManagement.WriteLn() : Error while writing in file "+file.getName());
             result = ERROR;
         }
         return result;
     }
 
-    public ArrayList<Double> ReadScoreFile(){
-        ArrayList<Double> result = new ArrayList<>();
+    //Read a file line by line and return it in an arraylist
+    private ArrayList<String>ReadFile(File file){
+        ArrayList<String>result = new ArrayList<>();
         try {
-            InputStream in = ctx.openFileInput(ScoreFileName);
-            if(in != null) {
-                InputStreamReader reader = new InputStreamReader(in);
-                BufferedReader br = new BufferedReader(reader);
-                String temp = "";
-                while ((temp = br.readLine()) != "") {
-                    if(temp != null)result.add(Double.valueOf(temp));
-                }
-                Log.e("Memory : ", "ReadScoreFile() : " + Integer.toString(result.size()));
-                br.close();
-                reader.close();
-                in.close();
-            }
-            Collections.sort(result);
+            FileInputStream fin = new FileInputStream(file);
+            InputStreamReader rfin = new InputStreamReader(fin);
+            BufferedReader brfin = new BufferedReader(rfin);
+            String str;
+            while((str = brfin.readLine()) != null)result.add(str);
+            brfin.close();
         }catch(IOException e){
-            Log.e("Memory : ","ReadScoreFile() : Error while reading file");
+            Log.e("MEMORY : ", "FileManagement.ReadLn() : Error while reading file "+file.getName());
+            result.clear();
+        }
+        return result;
+    }
+
+    //write value into FScore
+    public int WriteScore(String score){
+        Log.e("MEMORY : ","FileManagement.WriteScore() : "+score);
+        return WriteLn(FScore,score);
+    }
+
+    //read FScore
+    public ArrayList<Double> ReadScoreFile(){
+        Log.e("MEMORY : ","FileManagement.ReadScoreFile()");
+        ArrayList<Double> result = null;
+        ArrayList<String> temp = ReadFile(FScore);
+        try{
+           for(String str : temp) result.add(Double.valueOf(str));
+        }catch(NullPointerException e){
+            Log.e("MEMORY : ","FileManagement.ReadScoreFile() : Error while converting str to double");
+            result.clear();
             result = null;
         }
         return result;
     }
 
+    //Read FGamestate
     public int LoadGameState(){
         int result = ERROR;
-        Log.e("Memory : ","LoadGameState() : Error not yet implemented !");
+        Log.e("MEMORY : ","FileManagement.LoadGameState() : Error not yet implemented !");
         return result;
     }
 
+    //Save game state in FGamestate
     public int SaveGameState(int nb_coup, int mat[][]){
         int result = ERROR;
-        Log.e("Memory : ","SaveGameState() : Error not yet implemented !");
+        Log.e("MEMORY : ","FileManagement.SaveGameState() : Error not yet implemented !");
         return result;
     }
 }
--- app/src/main/java/projetandroidmaster1/memory/MainActivity.java
+++ app/src/main/java/projetandroidmaster1/memory/MainActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     public boolean debug = true;
 
@@ -21,20 +23,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
+        /*FM = new FileManagement(this);
+        FM.WriteScore("150");
+        ArrayList<Double> test = FM.ReadScoreFile();
+        toast(test.get(0).toString());*/
     }
-
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.e("Memory : ","onPause()");
+        Log.e("MEMORY : ","MainActivity.onPause()");
     }
 
     /***********************************************************************************************
-     * *********************************** functions ******************************************
+     * *********************************** functions ***********************************************
      * ********************************************************************************************/
 
     // Called when the player taps the Play button
@@ -43,24 +47,28 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //called when the player taps the setting button
+    public void launchSettingsActivity(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    //called when the player taps the score button
+    public void launchScoreActivity(View view) {
+        Intent intent = new Intent(this, ScoreActivity.class);
+        startActivity(intent);
+    }
+
+    //called when the player taps the quit button
+    public void QuitGame(View view) {
+        finish();
+    }
+
     public void toast(String input) {
         Context context = getApplicationContext();
         CharSequence text = input;
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
-    }
-
-    public void launchSettingsActivity(View view) {
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
-    }
-    public void launchScoreActivity(View view) {
-        Intent intent = new Intent(this, ScoreActivity.class);
-        startActivity(intent);
-    }
-
-    public void QuitGame(View view) {
-        finish();
     }
 }
